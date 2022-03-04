3fd267327ba84fa0c4f0cde32833074ea323a95e
LuizSouz404 <luiz.souz404@gmail.com>
2022-03-04 12:20:13 -0300

    Finished test websocket videochat

--- frontend/src/utils/setLocalStream.js
+++
@@ -1,6 +0,0 @@
-export const setLocalStream = ( stream, mirrorMode = true, videoRef ) => {
-    const localVidElem = videoRef;
-
-    localVidElem.srcObject = stream;
-    mirrorMode ? localVidElem.classList.add( 'mirror-mode' ) : localVidElem.classList.remove( 'mirror-mode' );
-}
